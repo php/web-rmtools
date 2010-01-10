6b7f085da8eef29a8f9e080131d1b64c5e14843c
Pierre Joye <pajoye@php.net>
2010-01-10 17:09:05 +0000

    - allow to override config by ENV vars

--- include/config.php
+++ include/config.php
@@ -1,8 +1,20 @@
 <?php
 namespace rmtools;
-set_include_path('.:' . __DIR__);
-define('MASTER_AUTH_TOKEN', getenv('MASTER_AUTH_TOKEN'));
-define('SVN_REPO_PATH', '/home/pierre/repos/phprepo/php');
-define('DB_PATH', '/home/pierre/public_html/rmtools/data');
-define('TPL_PATH', __DIR__ . '/../template');
 
+set_include_path('.:' . __DIR__);
+
+$rmtools_base = getenv('RMTOOLS_BASE_DIR');
+
+if (!$rmtools_base) {
+	define('MASTER_AUTH_TOKEN', getenv('MASTER_AUTH_TOKEN'));
+	define('SVN_REPO_PATH', '/home/web/rmtools.php.net/repos/php');
+	define('SVN_REPO_URL', 'http://svn.php.net/repository/php/php-src');
+	define('DB_PATH', '/home/web/rmtools.php.net/data');
+	define('TPL_PATH', __DIR__ . '/../template');
+} else {
+	define('MASTER_AUTH_TOKEN', getenv('MASTER_AUTH_TOKEN'));
+	define('SVN_REPO_PATH', $rmtools_base . '/repos/php');
+	define('SVN_REPO_URL', 'http://svn.php.net/repository/php/php-src');
+	define('DB_PATH', $rmtools_base . '/data');
+	define('TPL_PATH', $rmtools_base . '/template');
+}
