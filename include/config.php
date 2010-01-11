b508ff7f5690b632105d7877b7a1505850428ac3
Pierre Joye <pajoye@php.net>
2010-01-11 00:41:54 +0000

    - simplify

--- include/config.php
+++ include/config.php
@@ -6,15 +6,12 @@ set_include_path('.:' . __DIR__);
 $rmtools_base = getenv('RMTOOLS_BASE_DIR');
 
 if (!$rmtools_base) {
-	define('MASTER_AUTH_TOKEN', getenv('MASTER_AUTH_TOKEN'));
-	define('SVN_REPO_PATH', '/home/web/rmtools.php.net/repos/php');
-	define('SVN_REPO_URL', 'http://svn.php.net/repository/php/php-src');
-	define('DB_PATH', '/home/web/rmtools.php.net/data');
-	define('TPL_PATH', __DIR__ . '/../template');
-} else {
-	define('MASTER_AUTH_TOKEN', getenv('MASTER_AUTH_TOKEN'));
-	define('SVN_REPO_PATH', $rmtools_base . '/repos/php');
-	define('SVN_REPO_URL', 'http://svn.php.net/repository/php/php-src');
-	define('DB_PATH', $rmtools_base . '/data');
-	define('TPL_PATH', $rmtools_base . '/template');
+	$rmtools_base = '/home/web/rmtools.php.net';
 }
+
+define('MASTER_AUTH_TOKEN', getenv('MASTER_AUTH_TOKEN'));
+define('SVN_REPO_PATH', $rmtools_base . '/repos/php');
+define('SVN_REPO_URL', 'http://svn.php.net/repository/php/php-src');
+define('DB_PATH', $rmtools_base . '/data');
+define('TPL_PATH', $rmtools_base . '/template');
+define('SNAPS_PATH', $rmtools_base . '/snaps');
