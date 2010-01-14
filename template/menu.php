d973f89ed0db18c5a697b0418d2fbcda496dfd25
Pierre Joye <pajoye@php.net>
2010-01-14 18:14:53 +0000

    - show the menu entry as well for release with no release branch

--- template/menu.php
+++ template/menu.php
@@ -4,12 +4,8 @@
 <?php $release = $base->getRelease($release_name); ?>
 <h2>Release <?php echo $release_name; ?><br /></h2>
 <ul>
-<?php if ( $release['release_branch'] ==  $release['dev_branch']) { ?>
-<li>Merge mgt not availabe (no release branch).</li>
-<?php } else { ?>
-<li><a href="index.php?mode=list&release=<?php echo $release_name; ?>">Merges (updated on <?php echo $release['last_update'];?>)</a></li>
+<li><a href="index.php?mode=list&release=<?php echo $release_name; ?>">Merges (updated on <?php echo $release['release_last_update'];?>)</a></li>
 <li><a href="index.php?mode=list&nojs=1">Merges (no js)</a></li>
-<?php } ?>
 <li><a href="">Builds</a></li>
 </ul>
 <?php } ?>
