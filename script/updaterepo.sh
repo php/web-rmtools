fab91fcbdf8750c0fa9097357376f5e2b4ecaab2
Pierre Joye <pajoye@php.net>
2010-01-19 20:27:06 +0000

    - add sh script for the cron

---
+++ script/updaterepo.sh
@@ -0,0 +1,3 @@
+php /home/web/rmtools.php.net/script/updaterelease.php all
+php /home/web/rmtools.php.net/script/snapshot.php all
+rm -rf /tmp/rmtools*
---
+++ script/updatesite.sh
@@ -0,0 +1,1 @@
+php /home/web/rmtools.php.net/script/updateusersite.php
