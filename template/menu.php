245ca5e7570d171d491924fff252131be2352d5a
Pierre Joye <pajoye@php.net>
2010-03-08 10:42:21 +0000

    - kill the no js backend

--- template/menu.php
+++ template/menu.php
@@ -5,7 +5,6 @@
 <h2>Release <?php echo $release_name; ?><br /></h2>
 <ul>
 <li><a href="index.php?mode=list&release=<?php echo $release_name; ?>">Merges (updated on <?php echo $release['release_last_update'];?>)</a></li>
-<li><a href="index.php?mode=list&nojs=1">Merges (no js)</a></li>
 <li><a href="">Builds</a></li>
 </ul>
 <?php } ?>
