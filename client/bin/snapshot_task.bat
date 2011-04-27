48121abf09a69c88dddc9b9c4d69597e2ff0e446
Pierre Joye <pajoye@php.net>
2011-04-27 12:03:55 +0000

    - use export again, enable trunk

--- client/bin/snapshot_task.bat
+++ client/bin/snapshot_task.bat
@@ -23,14 +23,14 @@ CALL d:\php-sdk\bin\phpsdk_setvars.bat
 
 rmdir /q /s %RMTOOLS_BASE_DIR% >> %LOG_FILE% 2<&1
 
-rem svn export --quiet --force https://svn.php.net/repository/web/php-rmtools/trunk/client %RMTOOLS_BASE_DIR% >> %LOG_FILE% 2<&1
-xcopy /s /e /y /i src\rmtools-client %RMTOOLS_BASE_DIR% >> %LOG_FILE% 2<&1
+svn export --quiet --force https://svn.php.net/repository/web/php-rmtools/trunk/client %RMTOOLS_BASE_DIR% >> %LOG_FILE% 2<&1
+rem xcopy /s /e /y /i  D:\php-sdk\src\rmtools-client %RMTOOLS_BASE_DIR% >> %LOG_FILE% 2<&1
 
 copy D:\php-sdk\rmtools.base\data\config\credentials_ftps.php %RMTOOLS_BASE_DIR%\data\config\ >> %LOG_FILE% 2<&1
 copy D:\php-sdk\rmtools.base\data\db\* %RMTOOLS_BASE_DIR%\data\db\ >> %LOG_FILE% 2<&1
 
-CALL d:\php-sdk\rmtools-client\bin\snap.bat php53>> %LOG_FILE% 2<&1
-rem d:\php-sdk\mtools-client\bin\snap.bat phptrunk
+CALL d:\php-sdk\rmtools-client\bin\snap.bat php53 >> %LOG_FILE% 2<&1
+CALL d:\php-sdk\rmtools-client\bin\snap.bat phptrunk >> %LOG_FILE% 2<&1
 
 copy %RMTOOLS_BASE_DIR%\data\db\* D:\php-sdk\rmtools.base\data\db\ >> %LOG_FILE% 2<&1
 del D:\php-sdk\locks\snaps.lock >> %LOG_FILE% 2<&1
