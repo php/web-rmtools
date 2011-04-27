fe637119fba01ba83133baa70ea2c25d53edccfe
Pierre Joye <pajoye@php.net>
2011-04-27 09:38:26 +0000

    - don't overwrite rmtools base env and use global lock dir

--- client/bin/snap.bat
+++ client/bin/snap.bat
@@ -11,10 +11,12 @@ SET VC9_X64_INCLUDE=%VC9_DIR%\VC\Include;C:\Program Files\Microsoft SDKs\Windows
 SET VC9_X64_LIB=%VC9_DIR%\VC\Lib\amd64;C:\Program Files\Microsoft SDKs\Windows\v6.1\Lib\x64;%VC9_DIR%\VC\ATLMFC\LIB\AMD64; 
 SET VC9_X64_PATH=%VC9_DIR%\VC\Bin\x86_amd64;%VC9_DIR%\VC\Bin;%VC9_DIR%\VC\vcpackages;%VC9_DIR%\Common7\IDE;C:\Program Files\Microsoft SDKs\Windows\v6.1\Bin;C:\WINDOWS\Microsoft.NET\Framework64\v3.5;C:\WINDOWS\Microsoft.NET\Framework\v3.5;C:\WINDOWS\Microsoft.NET\Framework64\v2.0.50727;C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727;C:\Perl\site\bin;C:\Perl\bin;C:\Program Files\PHP\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;c:\win2k3cd;C:\Program Files\cvsnt;C:\Program Files\WinSCP\;C:\Program Files\CVSNT\ 
 
+SET OLD_PATH=%PATH%
+
 SET PATH=%PATH%;%VC9_PATH%
 SET LIB=%VC9_LIB%
 SET INCLUDE=%VC9_INCLUDE%
-SET RMTOOLS_BASE_DIR=d:\php-sdk\rmtools.new
 SET BISON_SIMPLE=d:\php-sdk\bin\bison.simple
 
 d:\php-sdk\php\php.exe -d extension_dir=d:\php-sdk\php\ext -d extension=php_openssl.dll  -d extension=php_curl.dll %BAT_DIR%\..\script\snap.php %*
+SET PATH=%OLD_PATH%
--- client/bin/snapshot_task.bat
+++ client/bin/snapshot_task.bat
@@ -17,22 +17,23 @@ IF EXIST D:\php-sdk\locks\snaps.lock (
 	GOTO EXIT_LOCKED
 )
 
-ECHO running >> %LOG_FILE%
+ECHO running > D:\php-sdk\locks\snaps.lock 
 
 CALL d:\php-sdk\bin\phpsdk_setvars.bat
 
 rmdir /q /s %RMTOOLS_BASE_DIR% >> %LOG_FILE% 2<&1
 
-svn export --quiet --force https://svn.php.net/repository/web/php-rmtools/trunk/client %RMTOOLS_BASE_DIR% >> %LOG_FILE% 2<&1
+rem svn export --quiet --force https://svn.php.net/repository/web/php-rmtools/trunk/client %RMTOOLS_BASE_DIR% >> %LOG_FILE% 2<&1
+xcopy /s /e /y /i src\rmtools-client %RMTOOLS_BASE_DIR% >> %LOG_FILE% 2<&1
 
 copy D:\php-sdk\rmtools.base\data\config\credentials_ftps.php %RMTOOLS_BASE_DIR%\data\config\ >> %LOG_FILE% 2<&1
 copy D:\php-sdk\rmtools.base\data\db\* %RMTOOLS_BASE_DIR%\data\db\ >> %LOG_FILE% 2<&1
 
-d:\php-sdk\rmtools-client\bin\snap.bat php53 >> %LOG_FILE% 2<&1
+CALL d:\php-sdk\rmtools-client\bin\snap.bat php53>> %LOG_FILE% 2<&1
 rem d:\php-sdk\mtools-client\bin\snap.bat phptrunk
 
 copy %RMTOOLS_BASE_DIR%\data\db\* D:\php-sdk\rmtools.base\data\db\ >> %LOG_FILE% 2<&1
-del %RMTOOLS_BASE_DIR%\data\snaps.lock >> %LOG_FILE% 2<&1
+del D:\php-sdk\locks\snaps.lock >> %LOG_FILE% 2<&1
 
 :EXIT_LOCKED
 echo Done.>> %LOG_FILE%
