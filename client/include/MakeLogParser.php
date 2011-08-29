8a42a87a0d1628b51515cad01f9eb71a421db17b
Pierre Joye <pajoye@php.net>
2011-08-29 09:53:25 +0000

    - method to get the diff between two build/analyzer logs

--- client/include/MakeLogParser.php
+++ client/include/MakeLogParser.php
@@ -5,6 +5,7 @@ namespace rmtools;
 class MakeLogParser {
 	public $log;
 	public $stats;
+	public $diff = NULL;
 
 	function toHtml($title)
 	{
@@ -30,5 +31,26 @@ class MakeLogParser {
 		} else {
 			return NULL;
 		}
+	}
+
+	function diff($prev) {
+		 $result = array();
+
+		 foreach ($cur as $k => $v) {
+			  if (array_key_exists($k, $prev)) {
+					if (is_array($v)) {
+						 $tmp_ar = log_diff($v, $prev[$k]);
+						 if (count($tmp_ar)) { $result[$k] = $tmp_ar; }
+					} else {
+						 if ($v != $prev[$k]) {
+							  $result[$k] = $v;
+						 }
+					}
+			  } else {
+					$result[$k] = $v;
+			  }
+		 }
+
+		 return $result;
 	}
 }
