44c4acc059b4c868f6783d646bbfb4c01ee4bfb2
Anatol Belski <ab@php.net>
2013-09-24 14:29:10 +0200

    build next package from a scheduled task

---
+++ client/bin/pecl_build_next.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+echo pick the next PECL pkg and pass to pecl_build_all.bat 
+
+SET BAT_DIR=%~dp0
+
+cd c:\pecl-in-pkg
+
+for /r %%i in (*) do (
+	call %BAT_DIR%pecl_build_all.bat --upload --package=%%i
+	del %%i
+	goto ONLY_ONE	
+)
+
+:ONLY_ONE
+cd %BAT_DIR%
+echo .
+
