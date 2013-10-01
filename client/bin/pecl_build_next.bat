a031898475f2f20115cf6d316537e00a371c5ef1
Anatol Belski <ab@php.net>
2013-10-01 13:47:51 +0200

    extended pecl_build_next as documented

--- client/bin/pecl_build_next.bat
+++ client/bin/pecl_build_next.bat
@@ -10,16 +10,31 @@ cd c:\pecl-in-pkg
 for /r %%i in (*) do (
 	call %BAT_DIR%pecl_build_all.bat --upload --aggregate-mail --package=%%i
 	del %%i
-	goto ONLY_ONE	
+	goto ONLY_ONE
 )
 
+cd c:\pecl-in-pkg-nomail
+
+for /r %%i in (*) do (
+	call %BAT_DIR%pecl_build_all.bat --upload --package=%%i
+	del %%i
+	goto ONLY_ONE
+)
 
 cd c:\pecl-in-snap
 
 for /r %%i in (*) do (
+	call %BAT_DIR%pecl_build_all.bat --upload --is-snap --aggregate-mail --package=%%i
+	del %%i
+	goto ONLY_ONE
+)
+
+cd c:\pecl-in-snap-nomail
+
+for /r %%i in (*) do (
 	call %BAT_DIR%pecl_build_all.bat --upload --is-snap --package=%%i
 	del %%i
-	goto ONLY_ONE	
+	goto ONLY_ONE
 )
 
 :ONLY_ONE
