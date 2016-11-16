81b98cd79fa41f9c3ce555b841ce70f5e855a58b
Anatol Belski <ab@php.net>
2016-11-16 19:45:53 +0100

    rename var and add script path

--- bin/rmtools_setvars.bat
+++ bin/rmtools_setvars.bat
@@ -3,9 +3,11 @@
 set PHP_RMTOOLS_BIN_PATH=%~dp0
 set PHP_RMTOOLS_BIN_PATH=%PHP_RMTOOLS_BIN_PATH:~0,-1%
 
-for %%a in ("%PHP_RMTOOLS_BIN_PATH%") do set PHP_RMTOOLS_PATH=%%~dpa
+for %%a in ("%PHP_RMTOOLS_BIN_PATH%") do set PHP_RMTOOLS_ROOT_PATH=%%~dpa
 rem remove trailing slash
-set PHP_RMTOOLS_PATH=%PHP_RMTOOLS_PATH:~0,-1%
+set PHP_RMTOOLS_ROOT_PATH=%PHP_RMTOOLS_ROOT_PATH:~0,-1%
+
+set PHP_RMTOOLS_SCRIPT_PATH=%PHP_RMTOOLS_ROOT_PATH%\script
 
 rem set PHP_RMTOOLS_BASE_DIR=c:\php-sdk\rmtools-client
 
