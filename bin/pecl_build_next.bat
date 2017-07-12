3fcac8012ac059f00b89bc825e4bd53d1ae7a183
Anatol Belski <ab@php.net>
2017-07-12 21:07:44 +0200

    fix dir switching things

--- bin/pecl_build_next.bat
+++ bin/pecl_build_next.bat
@@ -13,75 +13,81 @@ if not exist %PHP_RMTOOLS_PECL_IN_PKG_PATH% (
 	echo %PHP_RMTOOLS_PECL_IN_PKG_PATH% does not exist
 	exit /b 3
 )
-cd %PHP_RMTOOLS_PECL_IN_PKG_PATH%
+pushd %PHP_RMTOOLS_PECL_IN_PKG_PATH%
 
 for /r %%i in (*) do (
 	call "%PHP_RMTOOLS_BIN_PATH%\pecl_build_all.bat" --upload --aggregate-mail --package=%%i
 	del %%i
 	goto ONLY_ONE
 )
+popd
 
 if not exist %PHP_RMTOOLS_PECL_IN_PKG_NOMAIL_PATH% (
 	echo %PHP_RMTOOLS_PECL_IN_PKG_NOMAIL_PATH% does not exist
 	exit /b 3
 )
-cd %PHP_RMTOOLS_PECL_IN_PKG_NOMAIL_PATH%
+pushd %PHP_RMTOOLS_PECL_IN_PKG_NOMAIL_PATH%
 
 for /r %%i in (*) do (
 	call "%PHP_RMTOOLS_BIN_PATH%\pecl_build_all.bat" --upload --package=%%i
 	del %%i
 	goto ONLY_ONE
 )
+popd
 
 if not exist %PHP_RMTOOLS_PECL_IN_SNAP_PATH% (
 	echo %PHP_RMTOOLS_PECL_IN_SNAP_PATH% does not exist
 	exit /b 3
 )
-cd %PHP_RMTOOLS_PECL_IN_SNAP_PATH%
+pushd %PHP_RMTOOLS_PECL_IN_SNAP_PATH%
 
 for /r %%i in (*) do (
 	call "%PHP_RMTOOLS_BIN_PATH%\pecl_build_all.bat" --upload --is-snap --aggregate-mail --package=%%i
 	del %%i
 	goto ONLY_ONE
 )
+popd
 
 if not exist %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PATH% (
 	echo %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PATH% does not exist
 	exit /b 3
 )
-cd %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PATH%
+pushd %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PATH%
 
 for /r %%i in (*) do (
 	call "%PHP_RMTOOLS_BIN_PATH%\pecl_build_all.bat" --upload --is-snap --package=%%i
 	del %%i
 	goto ONLY_ONE
 )
+popd
 
 if not exist %PHP_RMTOOLS_PECL_IN_SNAP_PRE_PATH% (
 	echo %PHP_RMTOOLS_PECL_IN_SNAP_PRE_PATH% does not exist
 	exit /b 3
 )
-cd %PHP_RMTOOLS_PECL_IN_SNAP_PRE_PATH%
+pushd %PHP_RMTOOLS_PECL_IN_SNAP_PRE_PATH%
 
 for /r %%i in (*) do (
 	call "%PHP_RMTOOLS_BIN_PATH%\pecl_snap_pre.bat" --upload --is-snap --aggregate-mail --package=%%i
 	del %%i
 	goto ONLY_ONE
 )
+popd
 
 if not exist %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PRE_PATH% (
 	echo %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PRE_PATH% does not exist
 	exit /b 3
 )
-cd %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PRE_PATH%
+pushd %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PRE_PATH%
 
 for /r %%i in (*) do (
 	call "%PHP_RMTOOLS_BIN_PATH%\pecl_snap_pre.bat" --upload --is-snap --package=%%i
 	del %%i
 	goto ONLY_ONE
 )
+popd
 
 :ONLY_ONE
-cd %PHP_RMTOOLS_BIN_PATH%
+popd
 echo .
 
