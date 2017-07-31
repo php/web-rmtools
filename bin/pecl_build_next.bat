f0831e59e17b7e3efea61433d69effc777702e52
Anatol Belski <ab@php.net>
2017-07-31 13:01:48 +0200

    Integrate the pre snaps and mass rebuild.
    See the comment about the extra adjustment about how to get it rolling.

--- bin/pecl_build_next.bat
+++ bin/pecl_build_next.bat
@@ -68,7 +68,7 @@ if not exist %PHP_RMTOOLS_PECL_IN_SNAP_PRE_PATH% (
 pushd %PHP_RMTOOLS_PECL_IN_SNAP_PRE_PATH%
 
 for /r %%i in (*) do (
-	call "%PHP_RMTOOLS_BIN_PATH%\pecl_snap_pre.bat" --upload --is-snap --aggregate-mail --package=%%i
+	call "%PHP_RMTOOLS_BIN_PATH%\pecl_build_discord.bat" --upload --is-snap --aggregate-mail --package=%%i
 	del %%i
 	goto ONLY_ONE
 )
@@ -81,7 +81,33 @@ if not exist %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PRE_PATH% (
 pushd %PHP_RMTOOLS_PECL_IN_SNAP_NOMAIL_PRE_PATH%
 
 for /r %%i in (*) do (
-	call "%PHP_RMTOOLS_BIN_PATH%\pecl_snap_pre.bat" --upload --is-snap --package=%%i
+	call "%PHP_RMTOOLS_BIN_PATH%\pecl_build_discord.bat" --upload --is-snap --package=%%i
+	del %%i
+	goto ONLY_ONE
+)
+popd
+
+if not exist %PHP_RMTOOLS_PECL_IN_PKG_MASS_REBUILD_PATH% (
+	echo %PHP_RMTOOLS_PECL_IN_PKG_MASS_REBUILD_PATH% does not exist
+	exit /b 3
+)
+pushd %PHP_RMTOOLS_PECL_IN_PKG_MASS_REBUILD_PATH%
+
+for /r %%i in (*) do (
+	call "%PHP_RMTOOLS_BIN_PATH%\pecl_build_discord.bat" --upload --aggregate-mail --package=%%i
+	del %%i
+	goto ONLY_ONE
+)
+popd
+
+if not exist %PHP_RMTOOLS_PECL_IN_PKG_MASS_REBUILD_NOMAIL_PATH% (
+	echo %PHP_RMTOOLS_PECL_IN_PKG_MASS_REBUILD_NOMAIL_PATH% does not exist
+	exit /b 3
+)
+pushd %PHP_RMTOOLS_PECL_IN_PKG_MASS_REBUILD_NOMAIL_PATH%
+
+for /r %%i in (*) do (
+	call "%PHP_RMTOOLS_BIN_PATH%\pecl_build_discord.bat" --upload --package=%%i
 	del %%i
 	goto ONLY_ONE
 )
