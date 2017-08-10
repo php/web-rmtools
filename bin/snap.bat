3f5bf6b1b005483aa1712a24d1404d1a6c0e00f3
Anatol Belski <ab@php.net>
2017-08-10 03:42:20 +0200

    fix snap.bat, c33d8befb0ae9788af2b5d09fc645e5de6dc1ccf was wrong
    it is a general snapshot build wrapper, not an x86 specialized one

--- bin/snap.bat
+++ bin/snap.bat
@@ -1,9 +1,13 @@
-@echo off
+@ECHO OFF
 
-if not exist %~dp0rmtools_setvars.bat (
-	echo RMTOOLS is not setup, create %~dp0rmtools_setvars.bat
+rem must be on the env already
+if "%PHP_SDK_ROOT_PATH%"=="" (
+	echo PHP SDK is not setup
 	exit /b 3
 )
 call %~dp0rmtools_setvars.bat
 
-call %PHP_RMTOOLS_PHP_SDK_ROOT_PATH%\phpsdk-vc15-x86.bat -t %PHP_RMTOOLS_BIN_PATH%\bin\snapshot_task.bat --task-args "phpmaster all"
+@ECHO ON
+call %PHP_SDK_PHP_CMD% %PHP_RMTOOLS_SCRIPT_PATH%\snap.php %*
+@ECHO OFF
+
