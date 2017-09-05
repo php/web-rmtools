bc921ece7ecb620e6ddbfd4b0fde2b3c0a21e740
Anatol Belski <ab@php.net>
2017-09-05 20:19:06 +0200

    Care about directories

--- include/PeclExt.php
+++ include/PeclExt.php
@@ -887,7 +887,7 @@ nodoc:
 		/* XXX implement the packaging of original fs structure from package.xml */
 		$zip_file = TMP_DIR . DIRECTORY_SEPARATOR . $this->getPackageName() . '.zip';
 		foreach ($files_to_zip as $file_to_zip) {
-			$zip_cmd = $this->zip_cmd . ' -9 -D -j ' . $zip_file . ' ' . $file_to_zip;
+			$zip_cmd = $this->zip_cmd . ' -9 ' . $zip_file . ' ' . $file_to_zip;
 			system($zip_cmd, $status);
 			if ($status) {
 				throw new \Exception("Couldn't zip files for '$zip_file'");
